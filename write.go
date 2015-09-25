5c22a775803ba2605f348e952dda27bb753b615f
muthusubbu459 <muthusubbu459@gmail.com>
2015-09-25 10:22:21 -0700

    Create write.go

---
+++ write.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+import "os"
+func main() {
+f := createFile("C:/users/muthusubbu459/GoWorkspace/src/maximum/defer.txt")
+defer closeFile(f)
+writeFile(f)
+}
+func createFile(p string) *os.File {
+fmt.Println("creating")
+f, err := os.Create(p)
+if err != nil {
+panic(err)
+}
+return f
+}
+func writeFile(f *os.File) {
+fmt.Println("writing")
+fmt.Fprintln(f, "data\n")
+f.WriteString("%tmuthu\n")
+}
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	f.Close()
+}
